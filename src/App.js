52083d4fcf4ecfb6703b291b4d3618d355cc87ff
Benjamin Semah <benjaminsemah@gmail.com>
2022-03-11 16:20:56 +0000

    Add movie component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import { useEffect} from 'react'
+import { useState, useEffect} from 'react'
 import './App.css';
+import MovieCard from './MovieCard';
 import SearchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com?apikey=e97e12c';
 
+const movie1 = {
+  "Title": "Spiderman",
+  "Year": "2010",
+  "imdbID": "tt1785572",
+  "Type": "movie",
+  "Poster": "N/A"
+}
+
 const App = () => {
+  const [movies, setMovies] = useState([]);
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data.Search)
+    setMovies(data.Search)
   }
 
   useEffect(() => {
@@ -24,8 +34,30 @@ const App = () => {
       <div className="search">
         <input 
           placeholder="Search for movies"
+          value="Superman"
+          onChange={() => {}}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => {}}
         />
       </div>
+
+      {
+        movies?.length > 0
+          ? (
+            <div className="container">
+              {movies.map((movie) => (
+                <MovieCard movie={movie} />
+              ))}
+            </div>
+          ) : (
+            <div className="empty">
+              <h2>No movies found</h2>
+            </div>
+          )
+      }
     </div>
   );
 }
