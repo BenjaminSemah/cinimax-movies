3c711ece48a62a85fa2cdf4c17e29ec8630c9acb
Benjamin Semah <benjaminsemah@gmail.com>
2022-03-14 12:49:45 +0000

    Change API_URL from http to https

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import MovieCard from './MovieCard';
 import SearchIcon from './search.svg';
 
-const API_URL = 'http://www.omdbapi.com?apikey=e97e12c';
+const API_URL = 'https://www.omdbapi.com?apikey=e97e12c';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
